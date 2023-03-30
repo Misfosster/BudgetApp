3339cac938d1310016ff53ddda6bc17d0227ba4d
cph-jm390 <90380942+cph-jm390@users.noreply.github.com>
2023-03-30 19:28:51 +0200

    working withdraw

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import React from "react";
-
-import { useState } from "react";
-
-import CurrencyExchange from "./CurrencyExchange.jsx";
-
+import React, { useState } from "react";
+import User from "./components/User";
+import Withdraw from "./components/Withdraw";
 
 function App() {
-    
-    const [users, setUsers] = useState([
-        { name: 'User1', balance: 500 },
-        { name: 'User2', balance: 1000 },
-        { name: 'User3', balance: 250 }
-    ]);
-    return (
-     <div>
-    
-      <h1>Budget App</h1>
-      
-      <CurrencyExchange/>
+  const [users, setUsers] = useState({
+    User1: {
+      name: "Alice",
+      balance: 100,
+    },
+    User2: {
+      name: "Bob",
+      balance: 200,
+    },
+    User3: {
+      name: "Charlie",
+      balance: 300,
+    },
+  });
+
+  const handleWithdraw = (userId, amount) => {
+    setUsers((prevUsers) => {
+      const userToUpdate = prevUsers[userId];
+      const newBalance = userToUpdate.balance - amount;
+      const updatedUser = { ...userToUpdate, balance: newBalance };
+      return { ...prevUsers, [userId]: updatedUser };
+    });
+  };
+
+  return (
+    <div>
+      <User user={users.User1} />
+      <User user={users.User2} />
+      <User user={users.User3} />
+      <Withdraw userId="User1" handleWithdraw={handleWithdraw} />
+      <Withdraw userId="User2" handleWithdraw={handleWithdraw} />
+      <Withdraw userId="User3" handleWithdraw={handleWithdraw} />
     </div>
   );
 }
