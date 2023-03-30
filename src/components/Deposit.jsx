28d5ea3145a2eb3695e2d95e63f18ddc88300eff
Emilio Lagunas <Cph-ec100@cphbusiness.dk>
2023-03-30 19:38:14 +0200

    test

---
+++ src/components/Deposit.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function Deposit(props) {
+    const [amount, setAmount] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const { userId, handleDeposit } = props;
+        handleDeposit(userId, amount);
+        setAmount("");
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Deposit:
+                    <input
+                        type="number"
+                        value={amount}
+                        onChange={(event) => setAmount(Number(event.target.value))}
+                    />
+                </label>
+                <button type="submit">Deposit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Deposit;
